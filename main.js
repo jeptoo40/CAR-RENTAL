96293e743b001ec038556ae05e37d708e0240237
jeptoo40 <161425979+jeptoo40@users.noreply.github.com>
2025-04-12 16:20:33 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,72 @@
+
+    const menuBtn = document.getElementById("menu-btn");
+    const navLinks = document.getElementById("nav-links");
+    const menuBtnIcon = menuBtn.querySelector("i");
+
+    menuBtn.addEventListener("click", (e) => {
+        navLinks.classList.toggle("open");
+      
+        const isOpen = navLinks.classList.contains("open");
+        menuBtnIcon.setAttribute("class",isOpen ? "ri-close-line" : "ri-menu-line");
+
+    });
+navLinks.addEventListener("click", (e) => {
+    navLinks.classList.remove("open");
+    menuBtnIcon.setAttribute("class", "ri-menu-line");
+})
+
+const scrollRevealOption = {
+    distance: "50px",
+    duration: 1000,
+    easing: "ease-in-out",
+    reset: true
+};
+
+
+ScrollReveal().reveal(".header__img", {
+    ...scrollRevealOption,
+    origin: "top"
+});
+
+ScrollReveal().reveal(".header__content h1", {
+    ...scrollRevealOption,
+    delay: 500
+});
+
+ScrollReveal().reveal(".header__content p", {
+    ...scrollRevealOption,
+    delay: 1000
+});
+
+ScrollReveal().reveal(".header__links", {
+    ...scrollRevealOption,
+    delay: 1500
+});
+
+ScrollReveal().reveal(".steps__card",{
+    ...scrollRevealOption,
+    interval: 500,
+});
+
+ScrollReveal().reveal(".service__img",{
+    ...scrollRevealOption,
+    origin: "left",
+});
+ScrollReveal().reveal(".service__content  .section__subheader",{
+    ...scrollRevealOption,
+    delay: 500,
+});
+ScrollReveal().reveal(".service__content  .section__subheader",{
+    ...scrollRevealOption,
+    delay: 1000,
+});
+ScrollReveal().reveal(".service__list li", {
+    ...scrollRevealOption,
+    delay: 1500,
+    interval: 500,
+});
+ScrollReveal().reveal(".experience__card", {
+    duration: 1000,
+    
+    interval: 500,
+});
